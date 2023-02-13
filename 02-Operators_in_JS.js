34c2f23febd1d4e9abb43dc15f3a26fd5c4cde5b
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-02-13 20:31:27 +0530

    Add files via upload

---
+++ 02-Operators_in_JS.js
@@ -0,0 +1,37 @@
+//Operators in JavaScript
+
+//Arithmetic operators
+let a=5
+let b=3
+console.log("a + b = ",a+b)  //addition
+console.log("a - b = ",a-b)  //subtraction
+console.log("a * b = ",a*b)  //multiplication
+console.log("a / b = ",a/b)   //division
+console.log("a % b = ",a%b)   //modules
+console.log("a ** b = ",a**b)   //exponential
+console.log("a++ = ",a++)  //here it will first print the value and then increment
+console.log("a-- = ",a--)   //here it will first print the value and then increment
+console.log("++a = ",++a)   //first increment and then print
+console.log("--a = ",--a)   //first decrement and then print
+
+//comparison operators
+let a1=2
+let a2=5
+//returns true and false values
+console.log("a1 == a2",a1==a2)
+console.log("a1 > a2",a1>a2)
+console.log("a1 < a2",a1<a2)
+console.log("a1 >= a2",a1>=a2)
+console.log("a1 <= a2",a1<=a2)
+console.log("a1 != a2",a1!=a2)
+console.log("a1 === a2",a1===a2)
+console.log("a1 !== a2",a1!==a2)
+
+//logical operators
+console.log( a1>a2 && a1!=a2)  //returns true if both true else returns false
+console.log( a1>a2 || a1!=a2)  //returns true if any one is true else returns false
+console.log(!true)
+console.log(!false)
+
+
+
