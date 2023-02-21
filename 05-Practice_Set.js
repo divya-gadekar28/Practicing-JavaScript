4b9611b9470dfd803de2d2a59ca7950c7a849000
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-02-21 19:55:14 +0530

    Add files via upload

---
+++ 05-Practice_Set.js
@@ -0,0 +1,84 @@
+const prompt = require("prompt-sync")({signint:true});
+//program to find area of triangle when base and height is given
+let base=prompt("enter value for base : ");
+base=Number.parseInt(base);
+let height=prompt("enter value for height : ");
+height=Number.parseInt(height);
+let area=(base * height)/2;
+area=Number.parseInt(area);
+console.log("Area of triangle is : ",area);
+
+
+//program to find area of triangle when all 3 sides are given
+let side1=prompt("enter value of side1 :");
+side1=Number.parseInt(side1);
+let side2=prompt("enter value of side2 :");
+side2=Number.parseInt(side2);
+let side3=prompt("enter value of side3 :");
+side3=Number.parseInt(side3);
+/*herron's formula :-
+s = (a+b+c)/2
+area = √(s((s-a)*(s-b)*(s-c))) */
+let s = (side1+side2+side3)/2;   //calculating semi-perimeter
+s=Number.parseInt(s);
+let area1=(s*((s-side1)*(s-side2)*(s-side3)));
+area1=Math.sqrt(area1);
+console.log("Area of triangle is :",area1);
+
+
+//program to swap 2 variables
+let num1=prompt("enter number1 : ");
+let num2=prompt("enter number2 : ");
+let temp;
+console.log("before swapping :-")
+console.log("number1 =",num1);
+console.log("number2 =",num2);
+temp=num1;
+num1=num2;
+num2=temp;
+console.log("after swapping :-")
+console.log("number1 =",num1);
+console.log("number2 =",num2);
+
+
+//program to display whether the alphabet is vowel or consonent
+let ch=prompt("enter an alphabet : ");
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+console.log(ch,"is a vowel");
+else if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+console.log(ch,"is a vowel");
+else
+console.log(ch,"is a consonent");
+
+
+//program to display table of number
+let n=prompt("enter the number : ")
+n=Number.parseInt(n);
+console.log("*** table of ",n,"***");
+let result;
+for(let i=1;i<=10;i++){
+    result=n*i;
+    console.log(n,"*",i,"=",result);
+
+
+// program to find the factors of an integer
+let num = prompt('Enter a positive number: ');
+console.log("The factors of",num,"is:");
+// looping through 1 to num
+for(let i = 1; i <= num; i++) {
+// check if number is a factor
+if(num % i == 0) {
+console.log(i);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
