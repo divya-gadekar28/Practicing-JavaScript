7ff49ecfb050aaf7ca697c01ec093299a68382e9
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-03-01 22:37:33 +0530

    Update 05-Practice_Set.js

--- 05-Practice_Set.js
+++ 05-Practice_Set.js
@@ -71,6 +71,19 @@ for(let i = 1; i <= num; i++) {
     }
 }
 
+//program to find max of 3 numbers
+let n1=100;
+let n2=5;
+let n3=54;
+if(n1>n2 && n1>n3){
+    console.log(n1," is maximum");
+}
+else if(n2>n3 && n2>n1){
+    console.log(n2," is maximum");
+}
+else{
+    console.log(n3,"is maximum");
+}
 
 
 
